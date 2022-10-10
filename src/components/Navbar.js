6e1169bab1e05362050eb09a691f08f323a266fd
gurumurthi97 <guru13041997@gmail.com>
2022-10-10 12:31:54 +0530

    completed Navbar css and html part

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,81 @@
 import React from "react";
-
+import { useState } from "react";
+const NavLink = {
+  textAlign: "center",
+  fontSize: 12,
+  minWidth: 60,
+  padding: 6,
+  marginLeft: 10,
+  marginRight: 10,
+};
+const selected = {
+  borderBottom: "1.5px solid #555555",
+};
 function Navbar() {
+  const [text, setText] = useState("");
   return (
     <div
       style={{
         backgroundColor: "white",
-        height: 55,
+        height: 52,
         width: "100%",
-        padding: "10px",
+        position: "fixed",
       }}
     >
-      <div>
-        <img src="linkedinlogo.png" alt="logo" style={{ height: "40" }} />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          maxWidth: 900,
+        }}
+      >
+        <div>
+          <img src="linkedinlogo.png" alt="logo" style={{ height: 32 }} />
+        </div>
+        <div style={{ flex: 1 }}>
+          <input
+            placeholder="Search"
+            style={{
+              height: 32,
+              width: 300,
+              paddingLeft: 20,
+              border: "0px",
+              borderRadius: 5,
+              backgroundColor: "#EEF3F8",
+              marginLeft: "10px",
+            }}
+            value={text}
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+          />
+        </div>
+        <div style={NavLink}>
+          <img src="home.png" alt="home" style={{ height: 20 }} />
+          <br />
+          <span>Home</span>
+        </div>
+        <div style={NavLink}>
+          <img src="home.png" alt="home" style={{ height: 20 }} />
+          <br />
+          <span>My Network</span>
+        </div>
+        <div style={NavLink}>
+          <img src="home.png" alt="home" style={{ height: 20 }} />
+          <br />
+          <span>Jobs</span>
+        </div>
+        <div style={NavLink}>
+          <img src="home.png" alt="home" style={{ height: 20 }} />
+          <br />
+          <span>Messageing</span>
+        </div>
+        <div style={NavLink}>
+          <img src="home.png" alt="home" style={{ height: 20 }} />
+          <br />
+          <span>Notification</span>
+        </div>
       </div>
     </div>
   );
