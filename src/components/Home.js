f34b6274035ede898c5f9b018ae0b2b326f57869
gurumurthi97 <guru13041997@gmail.com>
2022-10-10 20:15:19 +0530

    completed fixed part also

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Post from "./Post";
+import Profile from "./Profile";
+import Fixed from "./Fixed";
 function Home() {
   // const [data, setData] = useState({});
   // const [loading,setLoading]=useState(false);
@@ -13,13 +15,24 @@ function Home() {
   //      })
   // },[]);
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        maxWidth: "1200px",
+        margin: "auto",
+      }}
+    >
+      <div style={{ padding: 10 }}>
+        <Profile />
+        <Fixed />
+      </div>
       {/* {loading ? (
         <div>Loading</div>
       ) : ( */}
       <div>
         {/* {data.arr.map((d) => { */}
         {/* return ( */}
+
         <Post
           author={{
             name: "Dylan Sanusi-Goh",
@@ -32,42 +45,61 @@ function Home() {
           time="1d"
           likeCount={300}
         />
+        <Post
+          author={{
+            name: "Dylan Sanusi-Goh",
+            avatar:
+              "https://media-exp1.licdn.com/dms/image/C5603AQFm6ACatS_3yg/profile-displayphoto-shrink_200_200/0/1616742471486?e=1671062400&v=beta&t=U2OZGKcozoSQy0NyX26i1fxGzEh5NBw9IAQ-svV1kWU",
+            title:
+              "Intelligent Technologies Engineer | AFR Top 100 Future Leader",
+          }}
+          content="I'm experienced and passionate about developing disruptive technologies - notably having delivered impact across large scale transport to medium scale internet of things, cloud, software development, data applications, and autonomous robotics projects .. 🥰."
+          time="1d"
+          likeCount={300}
+        />
+
+        <Post
+          author={{
+            name: "Dylan Sanusi-Goh",
+            avatar:
+              "https://media-exp1.licdn.com/dms/image/C5603AQFm6ACatS_3yg/profile-displayphoto-shrink_200_200/0/1616742471486?e=1671062400&v=beta&t=U2OZGKcozoSQy0NyX26i1fxGzEh5NBw9IAQ-svV1kWU",
+            title:
+              "Intelligent Technologies Engineer | AFR Top 100 Future Leader",
+          }}
+          content="I'm experienced and passionate about developing disruptive technologies - notably having delivered impact across large scale transport to medium scale internet of things, cloud, software development, data applications, and autonomous robotics projects .. 🥰."
+          time="1d"
+          likeCount={300}
+        />
+        <Post
+          author={{
+            name: "Dylan Sanusi-Goh",
+            avatar:
+              "https://media-exp1.licdn.com/dms/image/C5603AQFm6ACatS_3yg/profile-displayphoto-shrink_200_200/0/1616742471486?e=1671062400&v=beta&t=U2OZGKcozoSQy0NyX26i1fxGzEh5NBw9IAQ-svV1kWU",
+            title:
+              "Intelligent Technologies Engineer | AFR Top 100 Future Leader",
+          }}
+          content="I'm experienced and passionate about developing disruptive technologies - notably having delivered impact across large scale transport to medium scale internet of things, cloud, software development, data applications, and autonomous robotics projects .. 🥰."
+          time="1d"
+          likeCount={300}
+        />
+        <Post
+          author={{
+            name: "Dylan Sanusi-Goh",
+            avatar:
+              "https://media-exp1.licdn.com/dms/image/C5603AQFm6ACatS_3yg/profile-displayphoto-shrink_200_200/0/1616742471486?e=1671062400&v=beta&t=U2OZGKcozoSQy0NyX26i1fxGzEh5NBw9IAQ-svV1kWU",
+            title:
+              "Intelligent Technologies Engineer | AFR Top 100 Future Leader",
+          }}
+          content="I'm experienced and passionate about developing disruptive technologies - notably having delivered impact across large scale transport to medium scale internet of things, cloud, software development, data applications, and autonomous robotics projects .. 🥰."
+          time="1d"
+          likeCount={300}
+        />
+
         {/* ); })} */}
       </div>
       {/* )} */}
-      <div>
-        {/* {data.arr.map((d) => { */}
-        {/* return ( */}
-        <Post
-          author={{
-            name: "Pincj ksik",
-            avatar:
-              "https://media-exp1.licdn.com/dms/image/C5603AQFm6ACatS_3yg/profile-displayphoto-shrink_200_200/0/1616742471486?e=1671062400&v=beta&t=U2OZGKcozoSQy0NyX26i1fxGzEh5NBw9IAQ-svV1kWU",
-            title:
-              "Intelligent Technologies Engineer | AFR Top 100 Future Leader",
-          }}
-          content="I'm experienced and passionate about developing disruptive technologies - notably having delivered impact across large scale transport to medium scale internet of things, cloud, software development, data applications, and autonomous robotics projects .. 🥰."
-          time="1d"
-          likeCount={300}
-        />
-        {/* ); })} */}
-      </div>
-      <div>
-        {/* {data.arr.map((d) => { */}
-        {/* return ( */}
-        <Post
-          author={{
-            name: "Dylan Sanusi-Goh",
-            avatar:
-              "https://media-exp1.licdn.com/dms/image/C5603AQFm6ACatS_3yg/profile-displayphoto-shrink_200_200/0/1616742471486?e=1671062400&v=beta&t=U2OZGKcozoSQy0NyX26i1fxGzEh5NBw9IAQ-svV1kWU",
-            title:
-              "Intelligent Technologies Engineer | AFR Top 100 Future Leader",
-          }}
-          content="I'm experienced and passionate about developing disruptive technologies - notably having delivered impact across large scale transport to medium scale internet of things, cloud, software development, data applications, and autonomous robotics projects .. 🥰."
-          time="1d"
-          likeCount={300}
-        />
-        {/* ); })} */}
+      <div style={{ padding: 10 }}>
+        <Profile />
       </div>
     </div>
   );
